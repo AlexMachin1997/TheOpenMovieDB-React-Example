77df9b4de6df1da783d64aad77be86436af1aa7e
AlexMachin1997 <AlexMachin1997@gmail.com>
2020-06-21 01:06:12 +0100

    Updated the link stories

--- src/components/Link/index.stories.js
+++ src/components/Link/index.stories.js
@@ -1,41 +1,32 @@
 import React from 'react';
 
 import Link from './index';
-import { StoryPreview } from './Link';
+import Preview from '../Blocks/Storybook/Preview';
 
-export const DefaultLink = () => (
-	<StoryPreview>
-		<Link />
-	</StoryPreview>
-);
+export const DefaultLink = () => <Preview content={<Link />} />;
 
-export const CustomLink = () => (
-	<StoryPreview>
-		<Link href="https://www.facebook.com/" />
-	</StoryPreview>
-);
-export const CustomContentText = () => (
-	<StoryPreview>
-		<Link content="Visit Facebook" />
-	</StoryPreview>
-);
+export const CustomLink = () => <Preview content={<Link href="https://www.facebook.com/" />} />;
+
+export const CustomContentText = () => <Preview content={<Link content="Visit Facebook" />} />;
 
 export const CustomContentJSX = () => (
-	<StoryPreview>
-		<Link content={<p style={{ margin: 0, fontSize: '1rem' }}>Custom JSX</p>} colour="red" />
-	</StoryPreview>
+	<Preview
+		content={
+			<Link content={<p style={{ margin: 0, fontSize: '1rem' }}>Custom JSX</p>} colour="red" />
+		}
+	/>
 );
 
 export const CustomNewTab = () => (
-	<StoryPreview>
-		<Link href="https://www.facebook.com/" newTab label="Facebook Social Link" />
-	</StoryPreview>
+	<Preview
+		content={<Link href="https://www.facebook.com/" newTab label="Facebook Social Link" />}
+	/>
 );
 
 export const CustomLabel = () => (
-	<StoryPreview>
-		<Link href="https://www.facebook.com/" newTab label="Facebook Social Link" />
-	</StoryPreview>
+	<Preview
+		content={<Link href="https://www.facebook.com/" newTab label="Facebook Social Link" />}
+	/>
 );
 
 export default {
