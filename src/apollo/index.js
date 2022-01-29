5c1509e3db30b2ef4bb4882052458153acb50713
AlexMachin1997 <AlexMachin1997@gmail.com>
2022-01-29 19:09:30 +0000

    chore: Removed uneeded Apollo functionality as it does what I wanted by default now

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,12 +1,11 @@
-import { ApolloClient } from '@apollo/client';
-import createCache from './cache';
-import createLink from './links';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 import settings from '../settings.json';
 
+const newApolloInMemoryCache = new InMemoryCache();
+
 const createApolloClient = new ApolloClient({
-	cache: createCache(),
-	link: createLink(),
+	cache: newApolloInMemoryCache,
 	connectToDevTools: process.env.NODE_ENV !== 'production',
 	uri: settings.REACT_APP_APOLLO_CLIENT_URI
 });
