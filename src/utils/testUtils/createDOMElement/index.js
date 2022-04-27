66fbdae85d886de1f39faf4a102c4235acbed358
AlexMachin1997 <AlexMachin1997@gmail.com>
2022-04-27 18:52:03 +0100

    Updated workflow dependencies

--- src/utils/testUtils/createDOMElement/index.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-
-const createDOMElement = (component, query) => {
-	render(component);
-	return document.body.querySelector(query);
-};
-
-export default createDOMElement;
