85fef65dac694fbebe04f0af51e9aee153880409
AlexMachin1997 <AlexMachin1997@gmail.com>
2022-12-01 16:42:28 +0000

    Fixed some eslint errors

--- src/components/Forms/DiscoverFilterSidebar/DiscoverFilterSidebar.stories.js
+++ src/components/Forms/DiscoverFilterSidebar/DiscoverFilterSidebar.stories.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Formik } from 'formik';
 
 import DiscoverFilterSidebar from './DiscoverFilterSidebar';
+import DiscoverFiltersFormData from '../../../utils/DiscoverFiltersFormData';
 
 const Template = ({ mediaType, resourceType, isAuthenticated }) => {
 	const formData = React.useMemo(
