9887222e7460043519bd0434f97694fb28255a16
AlexMachin1997 <AlexMachin1997@gmail.com>
2025-08-03 14:50:17 +0100

    Trying to speed up the load time for Storybook

--- apps/the-open-movie-database/.storybook/main.ts
+++ apps/the-open-movie-database/.storybook/main.ts
@@ -3,23 +3,13 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.stories.@(jsx|tsx)'],
 	addons: ['@storybook/addon-a11y', '@chromatic-com/storybook', '@storybook/addon-docs'],
-
 	framework: {
 		name: '@storybook/react-vite',
-
 		options: {
 			strictMode: true
 		}
 	},
-
-	docs: {},
-
-	// Load static assets, this allows stuff such as the favicon to be displayed in the browser tab
-	staticDirs: ['../public/'],
-
-	typescript: {
-		reactDocgen: 'react-docgen-typescript'
-	}
+	staticDirs: ['../public/']
 };
 
 export default config;
