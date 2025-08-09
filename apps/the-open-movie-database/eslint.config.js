c9fc20788f3edc0121aaa86b9903ca43f70aad47
AlexMachin1997 <AlexMachin1997@gmail.com>
2025-08-09 16:42:10 +0100

    Added jsdoc to make the eslint.config.js file for the web app type safe.

--- apps/the-open-movie-database/eslint.config.js
+++ apps/the-open-movie-database/eslint.config.js
@@ -1,5 +1,10 @@
 import { config as reactConfig } from '@repo/eslint-config/react';
 
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
 export default [
 	...reactConfig,
 	{
