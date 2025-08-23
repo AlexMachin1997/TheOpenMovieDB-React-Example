d47d76c9027583608ed1f0c0c58d8fd61e35f7ee
AlexMachin1997 <AlexMachin1997@gmail.com>
2025-08-23 15:58:14 +0100

    Dependency updates

--- packages/common-ui/.storybook/main.ts
+++ packages/common-ui/.storybook/main.ts
@@ -2,20 +2,12 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
 	stories: ['../src/components/**/*.stories.@(jsx|tsx)'],
-	addons: [
-		'@storybook/addon-a11y',
-		'@chromatic-com/storybook',
-		'@storybook/addon-docs'
-	],
+	addons: ['@storybook/addon-a11y', '@chromatic-com/storybook', '@storybook/addon-docs'],
 	framework: {
 		name: '@storybook/react-vite',
 		options: {
 			strictMode: true
 		}
-	},
-	viteFinal: async (config) => {
-		// Add any Vite configuration here if needed
-		return config;
 	}
 };
 
--- packages/common-ui/eslint.config.js
+++ packages/common-ui/eslint.config.js
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { config as reactConfig } from '@repo/eslint-config/react';
 
 /**
@@ -8,33 +5,36 @@ import { config as reactConfig } from '@repo/eslint-config/react';
  *
  * @type {import("eslint").Linter.Config[]}
  * */
-export default [...reactConfig, {
-    rules: {
-        // PERFORMANCE OPTIMIZATION: Disable the slowest rule (332ms, 29.8% of lint time)
-        // The import/extensions rule is very slow with complex path mappings
-        // TypeScript already enforces this when using moduleResolution node16/nodenext
-        'import/extensions': 'off',
+export default [
+	...reactConfig,
+	{
+		rules: {
+			// PERFORMANCE OPTIMIZATION: Disable the slowest rule (332ms, 29.8% of lint time)
+			// The import/extensions rule is very slow with complex path mappings
+			// TypeScript already enforces this when using moduleResolution node16/nodenext
+			'import/extensions': 'off',
 
-        // PERFORMANCE OPTIMIZATION: Replace slow TypeScript rule with faster alternative
-        // @typescript-eslint/no-unused-vars was taking 217ms (19.5% of lint time)
-        '@typescript-eslint/no-unused-vars': 'off',
-        'no-unused-vars': 'off', // Use TypeScript's built-in checking instead
+			// PERFORMANCE OPTIMIZATION: Replace slow TypeScript rule with faster alternative
+			// @typescript-eslint/no-unused-vars was taking 217ms (19.5% of lint time)
+			'@typescript-eslint/no-unused-vars': 'off',
+			'no-unused-vars': 'off', // Use TypeScript's built-in checking instead
 
-        // PERFORMANCE OPTIMIZATION: Disable expensive React rule
-        // react/display-name was taking 115ms (10.4% of lint time)
-        'react/display-name': 'off',
+			// PERFORMANCE OPTIMIZATION: Disable expensive React rule
+			// react/display-name was taking 115ms (10.4% of lint time)
+			'react/display-name': 'off',
 
-        // Disable slow import rules that TypeScript already checks (per typescript-eslint docs)
-        'import/named': 'off',
-        'import/namespace': 'off',
-        'import/default': 'off',
-        'import/no-named-as-default-member': 'off',
-        'import/no-unresolved': 'off',
-        'import/no-extraneous-dependencies': 'off',
-        // These are expensive - consider enabling only in CI
-        'import/no-named-as-default': 'off',
-        'import/no-cycle': 'off',
-        'import/no-unused-modules': 'off',
-        'import/no-deprecated': 'off'
-    }
-}, ...storybook.configs["flat/recommended"]];
+			// Disable slow import rules that TypeScript already checks (per typescript-eslint docs)
+			'import/named': 'off',
+			'import/namespace': 'off',
+			'import/default': 'off',
+			'import/no-named-as-default-member': 'off',
+			'import/no-unresolved': 'off',
+			'import/no-extraneous-dependencies': 'off',
+			// These are expensive - consider enabling only in CI
+			'import/no-named-as-default': 'off',
+			'import/no-cycle': 'off',
+			'import/no-unused-modules': 'off',
+			'import/no-deprecated': 'off'
+		}
+	}
+];
